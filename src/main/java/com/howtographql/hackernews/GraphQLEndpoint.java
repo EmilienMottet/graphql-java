bb3dbf55e007f402e063fb6ea85be6c3359d00c8
Emilien Mottet <emilien.mottet@michelin.com>
2018-07-24 15:37:57 +0200

    remove web.xml

--- src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
+++ src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
@@ -1,23 +1,10 @@
 package com.howtographql.hackernews;
 
 import com.coxautodev.graphql.tools.SchemaParser;
-import javax.servlet.annotation.WebServlet;
-import graphql.servlet.SimpleGraphQLServlet;
-import com.coxautodev.graphql.tools.SchemaParser;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import graphql.ExceptionWhileDataFetching;
-import graphql.GraphQLError;
 import graphql.schema.GraphQLSchema;
-import graphql.servlet.GraphQLContext;
 import graphql.servlet.SimpleGraphQLServlet;
+
+import javax.servlet.annotation.WebServlet;
 
 @WebServlet(urlPatterns = "/graphql")
 public class GraphQLEndpoint extends SimpleGraphQLServlet {
